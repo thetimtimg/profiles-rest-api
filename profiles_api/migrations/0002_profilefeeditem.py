00da24567c65e97830537434ddbeb6ec61186603
thetimtimg <timwgriggs@gmail.com>
2022-02-04 08:21:20 -0600

    Added user profile feed API

--- profiles_api/migrations/0002_profilefeeditem.py
+++ profiles_api/migrations/0002_profilefeeditem.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('status_text', models.CharField(max_length=255)),
-                ('created_on', models.DateTimeField(auto_now_add=True)),
+                ('creatd_on', models.DateTimeField(auto_now_add=True)),
                 ('user_profile', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -44,5 +44,5 @@ class ProfileFeedItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.ProfileFeedItem
-        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        fields = ('id', 'user_profile', 'status_text', 'create_on')
         extra_kwargs = {'user_profile': {'read_only': True}}
